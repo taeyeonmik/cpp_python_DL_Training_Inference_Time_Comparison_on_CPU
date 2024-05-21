1e0e47e698c24e450aed9a8ef574b085b5d1e6c7
taeyeonmik <bigkitekim@icloud.com>
2024-05-21 02:53:20 +0200

    update train method / add validation methiod

--- cpp/lib/ConvNet.hpp
+++ cpp/lib/ConvNet.hpp
@@ -6,59 +6,48 @@ using namespace std;
 
 class ConvNet : public torch::nn::Module {
 private:
-    int64_t wh[2] = {0, 0};
-    int64_t *whPtr = wh;
+    uint8_t k = 3;
+    uint8_t p = 1;
+    uint8_t s = 1;
+    uint8_t poolk = 2;
+    uint8_t pools = 1;
 
     torch::nn::Sequential convBlock1 {
-            torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 16, /*kernel_size=*/3).stride(1).padding(1)),
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 16, /*kernel_size=*/k).stride(s).padding(p)),
             torch::nn::BatchNorm2d(16),
             torch::nn::ReLU(),
-            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(1))
+            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(poolk).stride(pools))
     };
     torch::nn::Sequential convBlock2 {
-            torch::nn::Conv2d(torch::nn::Conv2dOptions(16, 32, /*kernel_size=*/3).stride(1).padding(1)),
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(16, 32, /*kernel_size=*/k).stride(s).padding(p)),
             torch::nn::BatchNorm2d(32),
             torch::nn::ReLU(),
-            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(1))
+            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(poolk).stride(pools))
     };
     torch::nn::Sequential convBlock3 {
-            torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, /*kernel_size=*/3).stride(1).padding(1)),
-            torch::nn::BatchNorm2d(32),
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, /*kernel_size=*/k).stride(s).padding(p)),
+            torch::nn::BatchNorm2d(64),
             torch::nn::ReLU(),
-            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(1))
+            torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(poolk).stride(pools))
     };
     torch::nn::Linear fc;
-
-
-protected:
-    void getOutputSize(torch::Tensor x) {
-        auto size = x.sizes();
-        *(whPtr + 0) = size[0];
-        *(whPtr + 1) = size[1];
-    }
 public:
     // default constructor
-    explicit ConvNet(int64_t numClasses) : fc(64, numClasses) {
+    explicit ConvNet(uint16_t numClasses, uint8_t wout, uint8_t hout) : fc(wout*hout*64, numClasses) {
         // register_module() is needed when using parameters() method later on
         register_module("convBlock1", convBlock1);
         register_module("convBlock2", convBlock2);
         register_module("convBlock3", convBlock3);
         register_module("fc", fc);
-        cout << "Net created" << endl;
-        // calculate final output shape
+        cout << "ConvNet is ready to train." << endl;
     }
-
     // forward
     torch::Tensor forward(torch::Tensor x){
         x = convBlock1->forward(x);
         x = convBlock2->forward(x);
         x = convBlock3->forward(x);
-        if (wh[0] == 0) { getOutputSize(x); }
-        x = x.view({-1, wh[0] * wh[1] * 64}); // flatten x
+        x = x.view({x.size(0), -1}); // flatten x
         return fc->forward(x);
     }
-
-    // save parameters
-    void save();
 };
 
--- cpp/lib/ConvNetClassifier.hpp
+++ cpp/lib/ConvNetClassifier.hpp
@@ -1,37 +1,124 @@
+#ifndef CONVNETCLASSIFIER_HPP
+#define CONVNETCLASSIFIER_HPP
+
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include <torch/script.h>
 #include <torch/torch.h>
 
 #include "ConvNet.hpp"
+#include "Dataset.hpp"
+#include "utils.hpp"
 
-using namespace std;
+using DataLoaderType = torch::data::StatelessDataLoader<torch::data::datasets::MapDataset<CustomDataset, torch::data::transforms::Stack<>>, torch::data::samplers::RandomSampler>;
 
 class ConvNetClassifier {
-private:
 public:
     torch::Device *device;
     ConvNet *model;
     torch::optim::Adam *optimizer;
-    vector<double> loss;
-    vector<double> accuracy;
+    std::vector<double> train_loss;
+    std::vector<double> valid_loss;
+    std::vector<double> valid_accuracy;
 
-    // counstructor
-    ConvNetClassifier(int64_t numClasses, double learningRate) {
+    // constructor
+    ConvNetClassifier(uint16_t numClasses, double learningRate) {
         device = new torch::Device(torch::cuda::is_available() ? torch::kCUDA : torch::kCPU);
-        cout << "ConvNet Classifier is being compiled...\n numClasses: " << numClasses << "\n" << "learningRate: " << learningRate << "\n" << "optimizer: Adam" << endl;
-        model = new ConvNet(numClasses);
+        cout << "Device: " << *device << endl;
+        cout << "ConvNet Classifier is being compiled...\nnumClasses: " << numClasses << "\n" << "learningRate: " << learningRate << "\n" << "optimizer: Adam" << endl;
+        std::tuple<uint8_t, uint8_t> wh_out = getOutputSize(3);
+        model = new ConvNet(numClasses, std::get<0>(wh_out), std::get<1>(wh_out));
         (*model).to(*device);
         optimizer = new torch::optim::Adam(
                 (*model).parameters(),
                 torch::optim::AdamOptions(learningRate)
         );
     }
-    void train() {
+    // constructor for inference
+    ConvNetClassifier(uint16_t numClasses, std::string modelPath) {
+        device = new torch::Device(torch::cuda::is_available() ? torch::kCUDA : torch::kCPU);
+        cout << "Device: " << *device << endl;
+        std::tuple<uint8_t, uint8_t> wh_out = getOutputSize(3);
+        model = new ConvNet(numClasses, std::get<0>(wh_out), std::get<1>(wh_out));
 
+        // Load model state
+        torch::serialize::InputArchive input_archive;
+        input_archive.load_from(modelPath);
+        model->load(input_archive);
+
+        cout << "ConvNet Classifier is compiled with \"model.pt\"" << endl;
+        (*model).to(*device);
     }
-    void evaluate() {
+    void train(DataLoaderType& train_dataloader, DataLoaderType& valid_dataloader, size_t num_epochs, uint16_t train_step) {
+        auto criterion = torch::nn::CrossEntropyLoss();
+        for (size_t epoch=0; epoch<num_epochs; ++epoch) {
+            size_t batch_idx = 0;
+            double total_loss = 0.0f;
+            auto start = std::chrono::high_resolution_clock::now();
+            for (torch::data::Example<>& batch : train_dataloader) {
+                auto data = batch.data.to(*device);
+                auto targets = batch.target.to(*device);
 
+                optimizer->zero_grad();
+                torch::Tensor output = model->forward(data);
+                torch::Tensor loss = criterion(output, targets);
+
+                loss.backward();
+                optimizer->step();
+
+                total_loss += loss.item<double>();
+                // Print loss
+                if (batch_idx++ % 10 == 0) {
+                    std::cout << "Train Epoch: ["<< epoch + 1 << "]\tStep: [" << batch_idx << "/" << train_step << "]\tLoss: " << loss.item<double>() << std::endl;
+                }
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = end - start;
+
+            double average_loss = total_loss / (double) batch_idx;
+            train_loss.push_back(total_loss);
+
+            // validation
+            size_t batch_val_idx = 0;
+            double total_val_loss = 0.0f;
+            double total_accuracy = 0.0f;
+            for (torch::data::Example<>& batch : valid_dataloader) {
+                auto data = batch.data.to(*device);
+                auto targets = batch.target.to(*device);
+                {
+                    torch::NoGradGuard no_grad;
+                    auto [val_loss, accuracy] = evaluate(*model, data, targets, criterion);
+                    total_val_loss += val_loss;
+                    total_accuracy += accuracy;
+                }
+                batch_val_idx += 1;
+            }
+            double average_val_loss = total_val_loss / (double) batch_val_idx;
+            double average_val_accuracy = total_accuracy / (double) batch_val_idx;
+            valid_loss.push_back(total_val_loss);
+            valid_accuracy.push_back(total_accuracy);
+            // Print total loss, average loss, average accuracy and time taken
+            std::cout << "Epoch [" << epoch + 1 << "/" << num_epochs << "], Total Train Loss: " << total_loss << ", Average Train Loss: " << average_loss << ", Time: " << elapsed.count() << " seconds" << std::endl;
+            std::cout << "Epoch [" << epoch + 1 << "/" << num_epochs << "], Total Valid Loss: " << total_val_loss << ", Average Valid Loss: " << average_val_loss << ", Average Valid Accuracy: " << average_val_accuracy << endl;
+
+            // save model
+            std::string model_path = "../model.pt";
+            torch::serialize::OutputArchive output_archive;
+            model->save(output_archive);
+            output_archive.save_to(model_path);
+        }
     }
+    // Dummy function for validation (replace with your actual validation function)
+    std::pair<double, double> evaluate(ConvNet& model, torch::Tensor input, torch::Tensor target, torch::nn::CrossEntropyLoss criterion) {
+        torch::Tensor output = model.forward(input);
+        torch::Tensor loss = criterion(output, target);
+        // Calculate accuracy
+        torch::Tensor pred = torch::argmax(output, 1);
+        double accuracy = (pred == target).sum().item<double>() / target.size(0);
 
+        return {loss.item<double>(), accuracy};
+    }
 };
+
+#endif
